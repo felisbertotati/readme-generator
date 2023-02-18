3e3254b5f3a050de73f1ec24895b8d1bb14f2c06
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-02-18 09:50:50 +0000

    - added in generate markdown the license, description and - table of contents

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## License:
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Description:
+  ${data.text}
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test](#test)
+  - [Additional Info](#github)
 
 `;
 }
